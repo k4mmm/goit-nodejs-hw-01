28054796aef033d2457a6ec8b01afbf5fe44f92a
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-11-08 12:51:52 +0200

    dlt yargs

--- index.js
+++ index.js
@@ -3,35 +3,12 @@ const {
   getContactById,
   listContacts,
   removeContact,
+  pathCheck,
 } = require("./contacts");
 
 const { Command } = require("commander");
 
-// const argv = require("yargs").argv;
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       listContacts();
-//       break;
-
-//     case "get":
-//       getContactById(id);
-//       break;
-
-//     case "add":
-//       addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
+invokeAction(argv);
 
 const program = new Command();
 program
